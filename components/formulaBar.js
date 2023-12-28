cd1b34d8a2a4f9efa05d34fba8d19ae88535c074
Charles Rethman <rethman@me.com>
2023-12-28 16:14:06 +1300

    Added in formula bar text box

---
+++ components/formulaBar.js
@@ -0,0 +1,6 @@
+import "../style.css";
+
+document.querySelector("#formula").innerHTML = `
+  <div>
+  </div>
+`
---
+++ functions/calculator.test.js
@@ -0,0 +1,9 @@
+import { expect } from "chai";
+
+describe("It must do basic arithmatic", () => {
+  describe("It must add", () => {
+    describe("It must add two numbers", () => {
+      expect(formuala("= 1 + 2 ")).to.equal(3);
+    });
+  });
+});
