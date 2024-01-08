33ea6f3e954a54bd3b2e523c4e6366ebe64a48da
Charles Rethman <rethman@me.com>
2024-01-08 13:38:08 +1300

    Trying to get the deploy to work ...again ... and again ...and again ...AND again

--- functions/calculator.js
+++ functions/calculator.js
@@ -1,7 +1,7 @@
 'use strict';
 
 import { toCoords } from './addressConverter.js';
-import { funcsList } from '../config.json' assert { type: 'json' };
+import { funcsList } from '../config.js';
 
 const calcFormula = (formula) => Function(`'use strict'; return (${formula.toString()})`)();
 
